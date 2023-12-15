daee3410d42f0eada40fd825ae2c0b56451d65e1
Sheriff Kurdi <sheriff.kurdi@gmail.com>
2023-12-15 23:29:59 +0200

    options pattern

---
+++ src/Kurdi.Inventory.Infrastructure/Configurations/settings/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Kurdi.Inventory.Infrastructure.Configurations.Settings;
+
+public class DatabaseSettings
+{
+    public const string DatabaseSection = "Database";
+    public string PostgresConnectionString { get; set; } = string.Empty;
+    public string SqlServerConnectionString { get; set; } = string.Empty;
+}
+
